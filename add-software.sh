81fab9339f734f3552cada4f260549dbffc552b4
alledog <alleydo@protonmail.com>
2021-02-01 17:46:51 +0300

    add dbeaver

--- add-software.sh
+++ add-software.sh
@@ -23,3 +23,7 @@ sudo apt-get install libreoffice libreoffice-kde libreoffice-pdfimport libreoffi
 wget -qO - https://packagecloud.io/AtomEditor/atom/gpgkey | sudo apt-key add
 sudo sh -c 'echo "deb [arch=amd64] https://packagecloud.io/AtomEditor/atom/any/ any main" > /etc/apt/sources.list.d/atom.list'
 sudo apt-get update && sudo apt-get install atom
+
+wget -O - https://dbeaver.io/debs/dbeaver.gpg.key | sudo apt-key add -
+echo "deb https://dbeaver.io/debs/dbeaver-ce /" | sudo tee /etc/apt/sources.list.d/dbeaver.list
+sudo apt-get update && sudo apt-get install dbeaver-ce

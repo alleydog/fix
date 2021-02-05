55fd1e1eac529fa5515c1aa28eb00db12e44dd86
alleydog <51403105+alleydog@users.noreply.github.com>
2021-02-05 12:45:25 +0300

    Update add-software.sh

--- add-software.sh
+++ add-software.sh
@@ -1,27 +1,32 @@
+pkcon refresh
+pkcon update
 pkcom install firefox-locale-ru transmission-qt curl mc links cmus mpv python3-distutils
 
-echo "deb http://repo.yandex.ru/yandex-disk/deb/ stable main" | sudo tee -a /etc/apt/sources.list.d/yandex-disk.list > /dev/null && wget http://repo.yandex.ru/yandex-disk/YANDEX-DISK-KEY.GPG -O- | sudo apt-key add - && sudo apt-get update && sudo apt-get install -y yandex-disk
+sudo add-apt-repository ppa:libreoffice/ppa
+sudo apt update
+sudo apt install -y libreoffice libreoffice-kde libreoffice-pdfimport libreoffice-help-ru libreoffice-l10n-ru myspell-ru hyphen-ru mythes-ru
 
-wget -q -O - https://repo.protonvpn.com/debian/public_key.asc | sudo apt-key add -
-sudo add-apt-repository 'deb https://repo.protonvpn.com/debian unstable main'
-sudo apt-get update && sudo apt-get install protonvpn
+echo "deb http://repo.yandex.ru/yandex-disk/deb/ stable main" | sudo tee -a /etc/apt/sources.list.d/yandex-disk.list > /dev/null
+wget http://repo.yandex.ru/yandex-disk/YANDEX-DISK-KEY.GPG -O- | sudo apt-key add -
+sudo apt update
+sudo apt install -y yandex-disk
 
 sudo apt install apt-transport-https curl gnupg
 curl -s https://brave-browser-apt-release.s3.brave.com/brave-core.asc | sudo apt-key --keyring /etc/apt/trusted.gpg.d/brave-browser-release.gpg add -
 echo "deb [arch=amd64] https://brave-browser-apt-release.s3.brave.com/ stable main" | sudo tee /etc/apt/sources.list.d/brave-browser-release.list
-sudo apt update && sudo apt install brave-browser
+sudo apt update
+sudo apt install -y brave-browser
 
 curl -sS https://download.spotify.com/debian/pubkey_0D811D58.gpg | sudo apt-key add -
 echo "deb http://repository.spotify.com stable non-free" | sudo tee /etc/apt/sources.list.d/spotify.list
-sudo apt-get update && sudo apt-get install spotify-client
-
-sudo add-apt-repository ppa:libreoffice/ppa && sudo apt-get update
-sudo apt-get install libreoffice libreoffice-kde libreoffice-pdfimport libreoffice-help-ru libreoffice-l10n-ru myspell-ru hyphen-ru mythes-ru
-
-wget -qO - https://packagecloud.io/AtomEditor/atom/gpgkey | sudo apt-key add
-sudo sh -c 'echo "deb [arch=amd64] https://packagecloud.io/AtomEditor/atom/any/ any main" > /etc/apt/sources.list.d/atom.list'
-sudo apt-get update && sudo apt-get install atom
+sudo apt update
+sudo apt install -y spotify-client
 
 wget -O - https://dbeaver.io/debs/dbeaver.gpg.key | sudo apt-key add -
 echo "deb https://dbeaver.io/debs/dbeaver-ce /" | sudo tee /etc/apt/sources.list.d/dbeaver.list
-sudo apt-get update && sudo apt-get install dbeaver-ce
+sudo apt update
+sudo apt install -y dbeaver-ce
+
+pkcon refresh
+pkcon update
+pkcon install yandex-disk brave-browser spotify-client dbeaver-ce

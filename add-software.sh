c04ead257ca3d9e0e8d9d71996d978a9b30e2959
alleydog <51403105+alleydog@users.noreply.github.com>
2021-04-23 14:52:12 +0300

    Update add-software.sh

--- add-software.sh
+++ add-software.sh
@@ -5,11 +5,6 @@ echo "deb http://repo.yandex.ru/yandex-disk/deb/ stable main" | sudo tee -a /etc
 wget http://repo.yandex.ru/yandex-disk/YANDEX-DISK-KEY.GPG -O- | sudo apt-key add -
 sudo apt update && sudo apt install -y yandex-disk
 
-sudo apt install apt-transport-https curl gnupg
-curl -s https://brave-browser-apt-release.s3.brave.com/brave-core.asc | sudo apt-key --keyring /etc/apt/trusted.gpg.d/brave-browser-release.gpg add -
-echo "deb [arch=amd64] https://brave-browser-apt-release.s3.brave.com/ stable main" | sudo tee /etc/apt/sources.list.d/brave-browser-release.list
-sudo apt update && sudo apt install -y brave-browser
-
 curl -sS https://download.spotify.com/debian/pubkey_0D811D58.gpg | sudo apt-key add -
 echo "deb http://repository.spotify.com stable non-free" | sudo tee /etc/apt/sources.list.d/spotify.list
 sudo apt update && sudo apt install -y spotify-client

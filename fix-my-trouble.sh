02ad62f9984c59039330be402dfe99696bbbbff4
alleydog <51403105+alleydog@users.noreply.github.com>
2022-04-03 07:44:11 +0300

    Update fix-my-trouble.sh

--- fix-my-trouble.sh
+++ fix-my-trouble.sh
@@ -1,29 +1,24 @@
-# fix-my-trouble.sh
-sudo nano /etc/systemd/system/fix-my-trouble.sh
-
-# >> fix-my-trouble.sh
 #!/bin/sh -e
-sudo setkeycodes 3a 42
+sudo touch /etc/systemd/system/fix-my-trouble.sh
 
-# fix-my-trouble.service
-sudo nano /etc/systemd/system/fix-my-trouble.service
+echo > #!/bin/sh -e
+echo >> sudo setkeycodes 3a 42
 
-# >> fix-my-trouble.service
-[Unit]
-Description=Fix My Trouble
-After=multi-user.target
+sudo touch /etc/systemd/system/fix-my-trouble.service
 
-[Service]
-Type=idle
-ExecStart=/etc/systemd/system/fix-my-trouble.sh
+echo > [Unit]
+echo >> Description=Fix My Trouble
+echo >> After=multi-user.target
 
-[Install]
-WantedBy=multi-user.target
+echo >> [Service]
+echo >> Type=idle
+echo >> ExecStart=/etc/systemd/system/fix-my-trouble.sh
 
-# chmod
+echo >> [Install]
+echo >> WantedBy=multi-user.target
+
 sudo chmod +x /etc/systemd/system/fix-my-trouble.sh
 sudo chmod 644 /etc/systemd/system/fix-my-trouble.service
 
-# systemctl
 sudo systemctl daemon-reload
 sudo systemctl enable /etc/systemd/system/fix-my-trouble.service

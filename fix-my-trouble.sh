009d535423c021f16a32c6d7dedacd5976891dd3
alleydog <51403105+alleydog@users.noreply.github.com>
2022-04-03 15:04:25 +0300

    Update fix-my-trouble.sh

--- fix-my-trouble.sh
+++ fix-my-trouble.sh
@@ -1,21 +1,21 @@
 #!/bin/sh -e
 sudo touch /etc/systemd/system/fix-my-trouble.sh
 
-echo > #!/bin/sh -e
-echo >> sudo setkeycodes 3a 42
+echo #!/bin/sh -e > /etc/systemd/system/fix-my-trouble.sh
+echo sudo setkeycodes 3a 42 >> /etc/systemd/system/fix-my-trouble.sh
 
 sudo touch /etc/systemd/system/fix-my-trouble.service
 
-echo > [Unit]
-echo >> Description=Fix My Trouble
-echo >> After=multi-user.target
+echo [Unit] > /etc/systemd/system/fix-my-trouble.service
+echo Description=Fix My Trouble >> /etc/systemd/system/fix-my-trouble.service
+echo After=multi-user.target >> /etc/systemd/system/fix-my-trouble.service
 
-echo >> [Service]
-echo >> Type=idle
-echo >> ExecStart=/etc/systemd/system/fix-my-trouble.sh
+echo [Service] >> /etc/systemd/system/fix-my-trouble.service
+echo Type=idle >> /etc/systemd/system/fix-my-trouble.service
+echo ExecStart=/etc/systemd/system/fix-my-trouble.sh >> /etc/systemd/system/fix-my-trouble.service
 
-echo >> [Install]
-echo >> WantedBy=multi-user.target
+echo [Install] >> /etc/systemd/system/fix-my-trouble.service
+echo WantedBy=multi-user.target >> /etc/systemd/system/fix-my-trouble.service
 
 sudo chmod +x /etc/systemd/system/fix-my-trouble.sh
 sudo chmod 644 /etc/systemd/system/fix-my-trouble.service

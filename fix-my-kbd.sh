e3bf2c4ac558dda1ca36a6fec93ba520a9e73d98
alleydog <51403105+alleydog@users.noreply.github.com>
2023-03-30 18:39:36 +0300

    Update and rename fix-my-trouble.sh to fix-my-kbd.sh

---
+++ fix-my-kbd.sh
@@ -0,0 +1,24 @@
+#!/bin/sh -e
+sudo touch /etc/systemd/system/fix-my-kbde.sh
+
+echo "#!/bin/sh -e" > /etc/systemd/system/fix-my-kbd.sh
+echo "sudo setkeycodes 3a 42" >> /etc/systemd/system/fix-my-kbd.sh
+
+sudo touch /etc/systemd/system/fix-my-kbd.service
+
+echo "[Unit]" > /etc/systemd/system/fix-my-kbd.service
+echo "Description=fix my kbd" >> /etc/systemd/system/fix-my-kbd.service
+echo "After=multi-user.target" >> /etc/systemd/system/fix-my-kbd.service
+
+echo "[Service]" >> /etc/systemd/system/fix-my-kbd.service
+echo "Type=idle" >> /etc/systemd/system/fix-my-kbd.service
+echo "ExecStart=/etc/systemd/system/fix-my-kbd.sh" >> /etc/systemd/system/fix-my-kbd.service
+
+echo "[Install]" >> /etc/systemd/system/fix-my-kbd.service
+echo "WantedBy=multi-user.target" >> /etc/systemd/system/fix-my-kbd.service
+
+sudo chmod +x /etc/systemd/system/fix-my-kbd.sh
+sudo chmod 644 /etc/systemd/system/fix-my-kbd.service
+
+sudo systemctl daemon-reload
+sudo systemctl enable /etc/systemd/system/fix-my-kbd.service

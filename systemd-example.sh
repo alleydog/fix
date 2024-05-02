b176d14a896590e28ef87f5a4f050dcc197016cf
alleydog <51403105+alleydog@users.noreply.github.com>
2024-05-02 08:29:42 +0300

    Update systemd-example.sh

--- systemd-example.sh
+++ systemd-example.sh
@@ -3,7 +3,7 @@
 sudo touch /etc/systemd/system/example.sh
 
 echo "#!/bin/sh -e" > /etc/systemd/system/example.sh
-echo "example" >> /etc/systemd/system/example.sh
+echo "sudo example" >> /etc/systemd/system/example.sh
 
 sudo touch /etc/systemd/system/example.service
 

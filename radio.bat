39ae03312dfce5c4d3ed22d05739582992c68fc7
alleydog <51403105+alleydog@users.noreply.github.com>
2024-05-07 10:20:34 +0300

    Update radio.bat

--- radio.bat
+++ radio.bat
@@ -1,3 +1,5 @@
 @echo off
-@ffmpeg -hide_banner -i "$SRC_URL" -c:a aac -b:a 32k -ac 2 -ar 48000 -f flv "$RTMP_URL/$STREAM_KEY"
-
+set SRC_URL=http://icecast.vgtrk.cdnvideo.ru/unost_aac_32kbps
+set RTMP_URL=rtmps://dc4-1.rtmp.t.me/s/
+set STREAM_KEY=0123456789:qwertyuiopasdfghjklzxc
+@ffmpeg -hide_banner -i "%SRC_URL%" -c:a aac -b:a 32k -ac 2 -ar 48000 -f flv "%RTMP_URL%%STREAM_KEY%"

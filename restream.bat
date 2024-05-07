c42cc521221ad9c313d41e5f1f80c2f38ede6f9b
alleydog <51403105+alleydog@users.noreply.github.com>
2024-05-07 12:34:22 +0300

    Update restream.bat

--- restream.bat
+++ restream.bat
@@ -2,4 +2,4 @@
 set SRC_URL=http://icecast.vgtrk.cdnvideo.ru/unost_aac_64kbps
 set RTMP_URL=rtmps://dc4-1.rtmp.t.me/s/
 set STREAM_KEY=0987654321:mnbvcxzlkjhgfdsapoiuytrewq
-@ffmpeg -hide_banner -i "%SRC_URL%" -c:a aac -b:a 64k -ac 2 -ar 48000 -f flv "%RTMP_URL%%STREAM_KEY%"
+ffmpeg -hide_banner -i "%SRC_URL%" -c:a aac -b:a 64k -ac 2 -ar 48000 -f flv "%RTMP_URL%%STREAM_KEY%"
